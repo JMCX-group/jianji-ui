bcde77aba4967af3f10423815f985a366af729fe
WHY <why@why.com>
2016-02-14 13:02:09 +0800

    登录页面排版完毕

--- login.php
+++ login.php
@@ -29,6 +29,32 @@
             left: 0;
             top: 0;
         }
+        .page-ext-opt-panel{
+            text-align: center;
+            color: rgb(142,161,204);
+        }
+        .page-ext-opt-divider{
+            display: inline-block;
+            width: 1px;
+            background-color: rgb(142,161,204);
+        }
+        .page-ext-opt-btn{
+            display: inline-block;
+            text-align: center;
+        }
+
+        .page-other-login-divider{
+            background-color: rgb(68,79,103);
+        }
+
+        .page-other-login-title{
+            color:rgb(68,79,103);
+            text-align: center;
+        }
+        .page-other-login-btn-group{
+            text-align: center;
+            font-size: 0;
+        }
 
         ::-webkit-input-placeholder { /* WebKit browsers */
             color:rgb(68,79,103);
@@ -42,7 +68,6 @@
         :-ms-input-placeholder { /* Internet Explorer 10+ */
             color:rgb(68,79,103);
         }
-
         input{
             outline:none;
             color: rgb(142,161,204);
@@ -100,10 +125,82 @@
             </div>
             <div class="cf-row" id="page-login-btn" data-cf-layout='{
                     "fontSize":38,
-                    "letterSpacing":20,
+                    "letterSpacing":12,
                     "height":86,
-                    "lineHeight":86
-                }'>登录</div>
+                    "lineHeight":86,
+                    "marginBottom":30
+                }'>&nbsp;登录</div>
+
+            <div class="cf-row page-ext-opt-panel" data-cf-layout='{
+                    "height":20,
+                    "lineHeight":20,
+                    "fontSize":0,
+                    "marginBottom":110
+                }'>
+                <span class="page-ext-opt-divider" data-cf-layout='{
+                        "height":20,
+                        "fontSize":20
+                    }'>&nbsp;</span>
+                <span class="page-ext-opt-btn" id="page-ext-opt-forget-pwd" data-cf-layout='{
+                        "height":20,
+                        "fontSize":20,
+                        "width":100
+                    }'>忘记密码</span>
+                <span class="page-ext-opt-divider" data-cf-layout='{
+                        "height":20,
+                        "fontSize":20
+                    }'>&nbsp;</span>
+                <span class="page-ext-opt-btn" id="page-ext-opt-register" data-cf-layout='{
+                        "height":20,
+                        "fontSize":20,
+                        "width":100
+                    }'>我要注册</span>
+                <span class="page-ext-opt-divider" data-cf-layout='{
+                        "height":20,
+                        "fontSize":20
+                    }'>&nbsp;</span>
+            </div>
+
+            <div class="cf-row page-other-login-panel">
+                <div class="cf-row">
+                    <div class="cf-col-x page-other-login-divider" data-cf-layout='{
+                            "width":144,
+                            "height":1,
+                            "marginTop":10
+                        }'></div>
+                    <div class="cf-col-x page-other-login-title" data-cf-layout='{
+                            "height":20,
+                            "lineHeight":20,
+                            "fontSize":20,
+                            "width":180
+                        }'>快速登录</div>
+                    <div class="cf-col-x page-other-login-divider" data-cf-layout='{
+                            "width":144,
+                            "height":1,
+                            "marginTop":10
+                        }'></div>
+                </div>
+                <div class="cf-row page-other-login-btn-group" data-cf-layout='{
+                        "paddingTop":30,
+                        "paddingBottom":75
+                    }'>
+                    <img src="img/temp/login/btn/weixin.png" data-cf-layout='{
+                            "width":86,
+                            "marginLeft":10,
+                            "marginRight":10
+                        }'>
+                    <img src="img/temp/login/btn/qq.png" data-cf-layout='{
+                            "width":86,
+                            "marginLeft":10,
+                            "marginRight":10
+                        }'>
+                    <img src="img/temp/login/btn/weibo.png" data-cf-layout='{
+                            "width":86,
+                            "marginLeft":10,
+                            "marginRight":10
+                        }'>
+                </div>
+            </div>
         </div>
     </div>
 <?php require_once(dirname(__FILE__).'/page_parts/common/js.php');?>
