b8e949dec09743509b427657eb0ad095d585333e
why <why>
2016-02-22 15:26:48 +0800

    上传推荐图片

---
+++ recomment_follow.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: why
+ * Date: 16/2/22
+ * Time: 下午2:53
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <title>简迹</title>
+    <?php require_once(dirname(__FILE__).'/page_parts/common/base_url.php');?>
+    <?php require_once(dirname(__FILE__).'/page_parts/common/fonts.php');?>
+    <?php require_once(dirname(__FILE__).'/page_parts/common/css.php');?>
+
+    <style>
+        #page-follow-list{
+            text-align: center;
+        }
+        .animated {
+            -webkit-animation-duration: 0.4s;
+            /*-webkit-animation-delay: 0.4s;*/
+
+            -moz-animation-duration: 0.4s;
+            /*-moz-animation-delay: 0.4s;*/
+
+            -ms-animation-duration: 0.4s;
+            /*-ms-animation-delay: 0.4s;*/
+
+            animation-duration: 0.4s;
+            /*animation-delay: 0.4s;*/
+        }
+    </style>
+</head>
+<body class="cf-invisible">
+    <div class="cf-wrap cf-wrap-no-bottom" id="page-follow-list">
+        <div class="cf-row" data-cf-layout='{"height":104}'></div>
+        <div class="cf-row" data-cf-layout='{
+                "marginBottom":50
+            }'>
+            <div class="cf-col-x" data-cf-layout='{
+                    "height":50,
+                    "lineHeight":50,
+                    "fontSize":0
+                }'><img src="img"></div>
+            <div class="cf-col-x"></div>
+        </div>
+    </div>
+</body>
+</html>
