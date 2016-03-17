f6c68cd30aaafab5168e77bb31d75c3b01bc3820
why <why>
2016-03-17 10:53:22 +0800

    封掉主页下面的足迹地图和档案室

--- page_parts/index/main_footer.php
+++ page_parts/index/main_footer.php
@@ -5,7 +5,7 @@
             "marginLeft":-320,
             "width":640
         }'>
-    <div class="cf-col-x cf-text-center" data-cf-layout='{
+    <div class="cf-col-x cf-text-center cf-invisible" data-cf-layout='{
                 "width":128
             }'>
         <a href="index.php?page_type=timeline&head=<?php echo $_GET['head']?>" class="cf-row" data-cf-layout='{
@@ -35,7 +35,7 @@
         <div class="cf-row page-operation-write">记录</div>
     </div>
 
-    <div class="cf-col-x cf-text-center" data-cf-layout='{
+    <div class="cf-col-x cf-text-center cf-invisible" data-cf-layout='{
                 "width":128
             }'>
         <a href="index.php?page_type=archive&head=<?php echo $_GET['head']?>" class="cf-row" data-cf-layout='{
